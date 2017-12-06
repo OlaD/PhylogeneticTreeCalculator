b8c17a511308384a6b9ea411ca253a22b14971ee
Ola D <pozeraczmaili@gmail.com>
2017-12-06 21:20:48 +0100

    Add graph visualisation

--- RootedPhylogeneticTreeCalculator/Form1.Designer.cs
+++ RootedPhylogeneticTreeCalculator/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.graphPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -60,11 +61,22 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
+            // graphPanel
+            // 
+            this.graphPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.graphPanel.Location = new System.Drawing.Point(96, 13);
+            this.graphPanel.Name = "graphPanel";
+            this.graphPanel.Size = new System.Drawing.Size(389, 367);
+            this.graphPanel.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(497, 392);
+            this.Controls.Add(this.graphPanel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel graphPanel;
     }
 }
 
--- RootedPhylogeneticTreeCalculator/Form1.cs
+++ RootedPhylogeneticTreeCalculator/Form1.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
 using System.Windows.Forms;
 
 namespace RootedPhylogeneticTreeCalculator
 {
     public partial class Form1 : Form
     {
+        GViewer viewer;
+
         public Form1()
         {
             InitializeComponent();
+            viewer = new GViewer();
 
+            // przykładowe drzewo
             TreeNode root = new TreeNode();
             TreeNode nodeABC = root.AddChild();
             nodeABC.AddChild("a");
@@ -30,6 +28,34 @@ namespace RootedPhylogeneticTreeCalculator
             label1.Text = nodeABC.Cluster.ToString();
             label2.Text = nodeDEF.Cluster.ToString();
             label3.Text = nodeEF.Cluster.ToString();
+
+            // przykładowy graf
+            Graph graph = new Graph();
+            graph.AddEdge("a", "b");
+            graph.AddEdge("a", "c");
+            graph.AddEdge("c", "d");
+
+            // przykładowa stylizacja węzłów i krawędzi
+            Node e = new Node("e");
+            e.Attr.Shape = Shape.Plaintext;
+            e.Label.FontSize = e.Label.FontSize - 5;
+            e.Label.FontColor = Color.BlueViolet;
+            graph.AddNode(e);
+
+            Edge edge = graph.AddEdge("c", "e");
+            edge.Attr.Color = Color.Pink;
+            edge.Attr.ArrowheadAtTarget = ArrowStyle.None;
+
+            ShowGraph(graph);
+        }
+
+        public void ShowGraph(Graph graph)
+        {
+            viewer.Graph = graph;
+            SuspendLayout();
+            viewer.Dock = DockStyle.Fill;
+            graphPanel.Controls.Add(viewer);
+            ResumeLayout();
         }
     }
 }
